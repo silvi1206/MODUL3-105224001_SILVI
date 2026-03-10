dccdfd32b7e405c2b5f76ffd938d22ff8e9ce23f
silvi1206 <silvianamaristha@gmail.com>
2026-03-10 17:04:12 +0700

    Latihan3

---
+++ latihanmodul3latihan3/Latihan 3/src/App.java
@@ -0,0 +1,16 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        for (int i = 0 ; i < 50 ; i++){
+            if(i % 3 == 0 || i % 5 == 0){
+                continue;
+            }
+            
+            System.out.println(i);
+        } 
+
+
+        
+    }
+}
